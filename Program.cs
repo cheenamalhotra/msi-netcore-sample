4cc855d2e6dea3bff7d6a6755477503aa87439de
Cheena Malhotra <v-chmalh@microsoft.com>
2019-07-14 19:37:11 -0700

    First Commit of sample app

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+
+namespace TestMSIAuthentication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 2){
+                throw new ArgumentNullException("args[Server, Database]");
+            }
+            MSITokenProvider provider = new MSITokenProvider();
+            String accessToken = provider.GetMSIAuthToken();
+            using (SqlConnection con = new SqlConnection($"Server={args[0]};Database={args[1]};"))
+            {
+                con.AccessToken = accessToken;
+                con.Open();
+                Console.WriteLine("Connected!");
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT @@VERSION";
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        reader.Read();
+                        Console.WriteLine(reader.GetString(0));
+                    }
+                }
+            }
+        }
+    }
+
+    class MSITokenProvider
+    {
+        public string GetMSIAuthToken()
+        {
+            // Build request to acquire managed identities for Azure resources token
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://database.windows.net/");
+            request.Headers["Metadata"] = "true";
+            request.Method = "GET";
+
+            try
+            {
+                // Call /token endpoint
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                // Pipe response Stream to a StreamReader, and extract access token
+                StreamReader streamResponse = new StreamReader(response.GetResponseStream());
+                string stringResponse = streamResponse.ReadToEnd();
+
+                MSIAccessToken tokenObject = JsonConvert.DeserializeObject<MSIAccessToken>(stringResponse);
+                return tokenObject.access_token;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            return null;
+        }
+    }
+
+    class MSIAccessToken
+    {
+        public string access_token { get; set; }
+        public string client_id { get; set; }
+        public long expires_in { get; set; }
+        public long expires_on { get; set; }
+        public long ext_expires_in { get; set; }
+        public long not_before { get; set; }
+        public string resource { get; set; }
+        public string token_type { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# Sample Console App to test MSI Authentication with .Net Core App
+
+This sample application can be used to test MSI Authentication from an **"Identity"** enabled Azure Resource. The application code acquires Access Token by making `HttpWebRequest` call to target Azure Services, and then creates `SqlConnection` instance with the access token acquired.
+
+### Packages Referenced:
+- Microsoft.Data.SqlClient 1.0.19189.1-Preview
+- Microsoft.Identity.Client 4.1.0
+- Newtonsoft.Json 12.0.2
+
+
+> NOTE: This code to fetch access token should not be used in production environments, please refer [Microsoft Documentation](https://docs.microsoft.com/en-us/azure/active-directory/managed-identities-azure-resources/how-to-use-vm-token) to handle HTTP Error scenarios.
+
+## Pre-Requisites
+- .Net Core SDK 2.1
+- Identity Feature enabled on Azure Resource where this application will run.
+- Access provided to Azure Resource (Virtual Machine/App Service/Function App) to connect to target Azure database. 
+    - Quick resolution: Run this query in Azure Database _(Login with Active Directory account)_:
+    
+        `CREATE USER [<AZURE_RESOURCE_NAME>] FROM EXTERNAL PROVIDER`
+
+## Steps to run this application:
+- Clone the repository
+- Run below commands:
+    - `dotnet restore`
+    - `dotnet msbuild`
+    - `dotnet .\bin\Debug\netcoreapp2.1\TestMSIAuthentication.dll <server> <database>`
+
+        e.g. `dotnet .\bin\Debug\netcoreapp2.1\TestMSIAuthentication.dll myserver.database.windows.net MyAzureDB`
+
+        Below response confirms connectivity established:
+
+        ```bash
+        PS C:\Users\dotnet\Documents\TestMSIAuthentication> dotnet bin\Debug\netcoreapp2.1\TestMSIAuthentication.dll <server> <database>
+        Connected!
+        Microsoft SQL Azure (RTM) - 12.0.2000.8
+            Jul  3 2019 10:02:53
+            Copyright (C) 2019 Microsoft Corporation
+        ```
